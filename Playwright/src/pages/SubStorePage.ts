d0ae65bce272ad0111696fe0119ed601b1231c5c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-02-14 16:36:11 +0530

    Update SubStorePage.ts

--- Playwright/src/pages/SubStorePage.ts
+++ Playwright/src/pages/SubStorePage.ts
@@ -2,50 +2,22 @@ import { Locator, Page } from "@playwright/test";
 
 export class SubStorePage {
   readonly page: Page;
-  private wardSupplyLink: Locator;
-  private substore: Locator;
-  private inventoryRequisitionTab: Locator;
-  private createRequisitionButton: Locator;
-  private targetInventoryDropdown: Locator;
-  private itemNameField: Locator;
-  private requestButton: Locator;
-  private successMessage: Locator;
-  private accountBtn: Locator;
-  private printButton: Locator;
-  private consumptionLink: Locator;
-  private newConsumptionBtn: Locator;
-  private inputItemName: Locator;
-  private saveBtn: Locator;
-  private successMessage1: Locator;
-  private reportLink: Locator;
-  private consumptionReport: Locator;
-  private subCategory: Locator;
-  private showReport: Locator;
-  private issueField: Locator;
+    public substore: {
+        substoreLink: Locator;
+        selectSubstore: Locator;
+        inventoryRequisition: Locator;
+        inventory: Locator;
+    }
 
-  constructor(page: Page) {
-    this.page = page;
-    this.wardSupplyLink = page.locator('');
-    this.substore = page.locator('');
-    this.inventoryRequisitionTab = page.locator('');
-    this.createRequisitionButton = page.locator('');
-    this.targetInventoryDropdown = page.locator('');
-    this.itemNameField = page.locator('');
-    this.requestButton = page.locator('');
-    this.successMessage = page.locator('');
-    this.accountBtn = page.locator('');
-    this.printButton = page.locator('');
-    this.consumptionLink = page.locator('');
-    this.newConsumptionBtn = page.locator('');
-    this.inputItemName = page.locator("");
-    this.saveBtn = page.locator("");
-    this.successMessage1 = page.locator('');
-    this.reportLink = page.locator('');
-    this.consumptionReport = page.locator('');
-    this.subCategory = page.locator('');
-    this.showReport = page.locator('');
-    this.issueField = page.locator("");
-  }
+    constructor(page: Page) {
+        this.page = page;
+        this.substore = {
+            substoreLink: page.locator(''),
+            selectSubstore: page.locator(''),
+            inventoryRequisition: page.locator(''),
+            inventory: page.locator(``),
+        }
+    }
 
   /**
      * @Test6
